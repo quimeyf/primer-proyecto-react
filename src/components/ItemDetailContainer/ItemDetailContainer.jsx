ad63138abe41c749e5a3caf356933d3d100137d4
Quimey Fernandez <quimeyanahifernandez@gmail.com>
2025-12-05 12:53:03 -0300

    alta de poductos

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,30 +1,16 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ItemDetail } from "../ItemDetail/ItemDetail";
+import { getProductById } from "../../services/products";
 
 export const ItemDetailContainer = () => {
     const [detail, setDetail] = useState({});
     const { id } = useParams();
 
     useEffect(() => {
-        fetch("/data/products.json")
-            .then((res) => {
-                if (!res.ok) {
-                    throw new Error("Hubo un problema al buscar productos");
-                }
-                return res.json();
-            })
-            .then((data) => {
-                const found = data.find(prod => prod.id === id)
-                if (found) {
-                    setDetail(found);
-                } else {
-                    throw new Error("Producto no encontrado");
-                }
-            })
-            .catch((err) => {
-                console.log(err);
-             });
+        getProductById(id)
+        .then((data) => setDetail(data))
+        .catch((err) => console.log(err))
     }, [id]);
 
     return (
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,34 +1,20 @@
 import { useState, useEffect } from "react";
 import { ItemList } from "../ItemList/ItemList";
 import { useParams } from "react-router-dom";
+import { getProducts } from "../../services/products";
 
 export const ItemListContainer = ({titulo}) => {
     const [products, setProducts] = useState([]);
     const {category} = useParams();
 
     useEffect(() => {
-        fetch("/data/products.json")
-        .then((res) => {
-            if(!res.ok){
-                throw new Error("Hubo un problema al buscar productos");
-            }
-            return res.json();
-        })
-        .then((data) => {
-            if (category) {
-                setProducts(data.filter((prod) => prod.category === category));
-            } else {
-                 setProducts(data);
-            }
-           
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+        getProducts(category)
+            .then((data) => setProducts(data))
+            .catch((err) => console.log(err))
      }, [category]);
 
     return (
-        <section>
+        <section className="container">
             <h1>{titulo}</h1>
             <ItemList list={products}/>
         </section>
